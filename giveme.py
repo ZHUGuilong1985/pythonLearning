eca7c072694753a985dfbaa6dfb7898524dd0479
ZHUGuilong1985 <otto377@163.com>
2024-02-18 22:37:28 +0800

    新建giveme

---
+++ giveme.py
@@ -0,0 +1,34 @@
+import os
+import time
+import pathlib
+
+def main():
+
+
+    None
+
+def new_vant():
+    '''
+    新建仓库
+    '''
+    None
+
+def get_Fold_from_vant():
+    '''
+    获取仓库
+    '''
+    None
+
+
+if __name__ == "__main__":
+    main()
+
+'''
+在文件夹内管理文件；
+删除文件时，可以选择：
+1. 删除具体文件，不删除文件夹，则可以保留记录；
+2. 删除文件夹。则删除所有记录；
+3. 建议是删除文件
+4. 保留的文件可以记录观看历史、评分、删除记录等等；
+
+'''
